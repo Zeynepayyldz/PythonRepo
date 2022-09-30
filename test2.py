fbce25babea06a21151cb3d2146e680d2723322c
Zeynepayyldz <ayyildizzeynep611@gmail.com>
2022-09-30 23:05:43 +0300

    aklindan sayi tut

--- test2.py
+++ test2.py
@@ -1,1 +1,9 @@
-print("hello zeynoo!")
+import random
+
+while True:
+    x=int(input("Bir sayı giriniz:"))
+    c=random.randint(0,100)
+    if c==x:
+        print("Şanslı şooopar!")
+        break
+    print(f"{x} girildi {c} üretildi. Soooooryy!")
